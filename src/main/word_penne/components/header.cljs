00ef6e754c53fef74fa2313a8344875c686a13a1
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-12-14 22:02:34 +0900

    navigation header

--- src/main/word_penne/components/header.cljs
+++ src/main/word_penne/components/header.cljs
@@ -1,7 +1,28 @@
-(ns word-penne.components.header)
+(ns word-penne.components.header
+  (:require [stylefy.core :as stylefy :refer [use-style]]))
+
+(def s-topnav
+  {:overflow "hidden"
+   :border-bottom "solid 1px #e9e9e9"
+   :position "fixed"
+   :top 0
+   :width "100%"
+   :display "flex"
+   :align-items "center"
+   :box-sizing "border-box"})
+
+(def s-link
+  {})
+
+(def s-search-container
+  {})
 
 (defn Header []
-  [:header "Word Penne"
-   [:div "検索"]
-   [:div "サインイン"]
-   [:div "サインアップ"]])
+  [:header (use-style s-topnav)
+   [:a {:href "/"} "Word Penne"]
+   [:div (use-style s-search-container)
+    [:input {:type "search" :placeholder "Search..." :name "search"}]
+    [:button {:type "submit"}
+     [:i {:class "material-icons"} "search"]]]
+   [:a {:href "#"} "Sign In"]
+   [:a {:href "#"} "Sign Up"]])
