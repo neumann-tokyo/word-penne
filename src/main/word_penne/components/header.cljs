1da074ac1e192ba0a9576ea07a5d9fd732d28488
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-12-14 21:06:58 +0900

    header

---
+++ src/main/word_penne/components/header.cljs
@@ -0,0 +1,7 @@
+(ns word-penne.components.header)
+
+(defn Header []
+  [:header "Word Penne"
+   [:div "検索"]
+   [:div "サインイン"]
+   [:div "サインアップ"]])
--- src/main/word_penne/views.cljs
+++ src/main/word_penne/views.cljs
@@ -3,15 +3,17 @@
             [stylefy.core :as stylefy :refer [use-style]]
             [bidi.bidi :refer [path-for]]
             [word-penne.subs :as subs]
-            [word-penne.routes :refer [routes]]))
+            [word-penne.routes :refer [routes]]
+            [word-penne.components.header :refer [Header]]))
 
-(def home-style
-  {:color "yellow"})
+(def s-home
+  {:color "red"})
 
+;; TODO I want to splite pages
 (defmulti view :handler)
 
 (defmethod view ::home [_]
-  [:div (use-style home-style) "Home"
+  [:div (use-style s-home) "Home"
    [:a {:href (path-for routes :word-penne.views/list)} "list"]])
 
 (defmethod view ::list [_]
@@ -27,10 +29,39 @@
 (defmethod view :default [_]
   [:div "404 Not Found"])
 
-(def main-panel-style
-  {:background-color "red"
-   :min-height "100vh"})
+;; ------------------
+
+;; TODO responsive
+(def s-main-panel
+  {:background-color "#F9F5F1"
+   :min-height "100vh"
+   :display "grid"
+   :grid-template-areas "'header header header' 'nav content side' 'footer footer footer'"
+   :grid-template-columns "200px 1fr 200px"
+   :grid-template-rows "auto 1fr auto"
+   :grid-gap "10px"})
+
+(def s-header
+  {:grid-area "header"})
+
+(def s-nav
+  {:grid-area "nav"
+   :margine-left "0.5rem"})
+
+(def s-main
+  {:grid-area "content"})
+
+(def s-aside
+  {:grid-area "side"
+   :margin-right "0.5rem"})
+
+(def s-footer
+  {:grid-area "footer"})
 
 (defn main-panel []
-  [:div (use-style main-panel-style) "Todo App"
-   [view @(re-frame/subscribe [::subs/current-route])]])
+  [:div (use-style s-main-panel)
+   [Header (use-style s-header)]
+   [:nav (use-style s-nav)]
+   [:main (use-style s-main) [view @(re-frame/subscribe [::subs/current-route])]]
+   [:aside (use-style s-aside)]
+   [:footer (use-style s-footer)]])
