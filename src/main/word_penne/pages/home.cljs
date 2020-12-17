e7a74064f86a23f4a8c748158aebcb86ccd5a6b9
kbaba1001 <kbaba1001@users.noreply.github.com>
2020-12-17 21:31:57 +0900

    add test button

--- src/main/word_penne/pages/home.cljs
+++ src/main/word_penne/pages/home.cljs
@@ -1,7 +1,13 @@
 (ns word-penne.pages.home
   (:require [word-penne.views :as v]
+            [word-penne.components.button :refer [Button]]
             [word-penne.components.word-cards-wrap :refer [WordCardsWrap]]))
 
 (defmethod v/view ::home [_]
   [:div
+   [:div
+    [:span "Tag1"]
+    ;; TODO 赤色のボタンにしたい
+    ;; TODO mobile では bottom navigation で Test を出したい
+    [Button {:href "#"} "Test"]]
    [WordCardsWrap]])
