a5fc6032a2d8c915d63f909111b28d63d79e6661
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-01-01 21:09:32 +0900

    remove re-frame-test

--- src/test/word_penne/events_test.cljs
+++ src/test/word_penne/events_test.cljs
@@ -2,7 +2,6 @@
   (:require [cljs.test :as t]
             ["node-fetch" :default fetch]
             [re-frame.core :as rf]
-            [day8.re-frame.test :as rf-test]
             [word-penne.events :as events]
             [word-penne.subs :as subs]
             [word-penne.firebase.init :refer [initialize-firebase]]))
@@ -15,14 +14,13 @@
        (t/async done (.then res #(done)))))})
 
 (t/deftest a-success-test
-  (rf-test/run-test-sync
-   (let [current-user (rf/subscribe [::subs/current-user])]
-     (initialize-firebase)
-     (rf/dispatch [::events/initialize-db])
-     (rf/dispatch [::events/set-current-user
-                   {:uid "uid1234"
-                    :email "foo@example.com"
-                    :photo-url "/images/account_circle-24px.svg"}])
-     (t/is (= (:uid @current-user) "uid1234"))
-     (t/is (= (:email @current-user) "foo@example.com"))
-     (t/is (= (:photo-url @current-user) "/images/account_circle-24px.svg")))))
+  (let [current-user (rf/subscribe [::subs/current-user])]
+    (initialize-firebase)
+    (rf/dispatch-sync [::events/initialize-db])
+    (rf/dispatch-sync [::events/set-current-user
+                       {:uid "uid1234"
+                        :email "foo@example.com"
+                        :photo-url "/images/account_circle-24px.svg"}])
+    (t/is (= (:uid @current-user) "uid1234"))
+    (t/is (= (:email @current-user) "foo@example.com"))
+    (t/is (= (:photo-url @current-user) "/images/account_circle-24px.svg"))))
