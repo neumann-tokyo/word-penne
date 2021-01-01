8dabe8b0d91d6a2f6bc9e3e7cb4d1cc905e073dd
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-01-01 14:00:40 +0900

    refactoring

--- src/test/word_penne/events_test.cljs
+++ src/test/word_penne/events_test.cljs
@@ -1,18 +1,13 @@
 (ns word-penne.events-test
   (:require [cljs.test :as t]
-            ["node-fetch" :default fetch]
-            [cljs.core.async :refer [go]]
-            [cljs.core.async.interop :refer-macros [<p!]]))
+            ["node-fetch" :default fetch]))
 
 ;; clean up the database
 (t/use-fixtures :each
   {:before
    (fn []
      (let [res (fetch "http://localhost:8081/emulator/v1/projects/word-penne/databases/(default)/documents" #js {:method "DELETE"})]
-       (t/async done
-                (go
-                  (<p! res)
-                  (done)))))})
+       (t/async done (.then res #(done)))))})
 
 (t/deftest a-failure-test
   (t/is (= 1 2)))
