fed4a9339c7cbbb6f2265dc30b31ca3557b8b144
shin1127 <ellendis1124@gmail.com>
2021-03-19 11:39:43 +0900

    feat: translated Cards->Card in hambarger menu

--- src/main/word_penne/components/navigation.cljs
+++ src/main/word_penne/components/navigation.cljs
@@ -5,7 +5,8 @@
             [word-penne.style.vars :refer [color layout-vars]]
             [word-penne.subs :as subs]
             [word-penne.events :as events]
-            [word-penne.routes :refer [routes]]))
+            [word-penne.routes :refer [routes]]
+            ["gettext.js" :as i18n]))
 
 (def s-navigation
   {:background (:main-background color)})
@@ -42,38 +43,42 @@
 ;; Pure CSS Hamburger menu
 ;; https://codepen.io/erikterwan/pen/EVzeRP
 (defn Navigation []
-  [:nav (use-style s-navigation)
-   [:input (use-style s-navigation-checkbox {:type "checkbox" :id "navigation-menu-checkbox" :name "navigation-menu-checkbox"})]
-   [:label {:for "navigation-menu-checkbox"}
-    [:span (use-style s-nav-icon {:class "material-icons-outlined"}) "menu"]]
-   [:div#navigation-menu (use-style s-opened-navigation)
-    [:div (use-style s-nav-close)]
-    [:a (use-style s-nav-link {:href "#"
-                               :data-testid "navigation__cards"
-                               :on-click (fn [e]
-                                           (.preventDefault e)
-                                           (re-frame/dispatch [::events/set-search-tag nil])
-                                           (re-frame/dispatch [::events/set-search-archive false])
-                                           (re-frame/dispatch [::events/navigate :word-penne.pages.home/home]))})
-     [:span {:class "material-icons-outlined"} "style"]
-     [:span (use-style s-nav-link-text) "Cards"]]
-    (doall (map-indexed
-            (fn [index tag]
-              [:a (use-style s-nav-link {:href "#" :key index
-                                         :data-testid (str "navigation__tags-" index)
-                                         :on-click (fn [e]
-                                                     (.preventDefault e)
-                                                     (re-frame/dispatch [::events/set-search-tag tag]))})
-               [:span {:class "material-icons-outlined"} "label"]
-               [:span (use-style s-nav-link-text) tag]])
-            @(re-frame/subscribe [::subs/tags])))
-    [:a (use-style s-nav-link {:href (path-for routes :word-penne.pages.tags/index)})
-     [:span {:class "material-icons-outlined"} "edit"]
-     [:span (use-style s-nav-link-text) "Edit tags"]]
-    [:a (use-style s-nav-link {:href "#"
-                               :data-testid "navigation__archive"
-                               :on-click (fn [e]
-                                           (.preventDefault e)
-                                           (re-frame/dispatch [::events/set-search-archive true]))})
-     [:span {:class "material-icons-outlined"} "archive"]
-     [:span (use-style s-nav-link-text) "Archive"]]]])
+  (let [n (i18n)]
+    (.setMessages n "messages" "jp" #js {"Cards" "カード"})
+    (.setLocale n "jp")
+    (js/console.log (.gettext n "Cards"))
+    [:nav (use-style s-navigation)
+     [:input (use-style s-navigation-checkbox {:type "checkbox" :id "navigation-menu-checkbox" :name "navigation-menu-checkbox"})]
+     [:label {:for "navigation-menu-checkbox"}
+      [:span (use-style s-nav-icon {:class "material-icons-outlined"}) "menu"]]
+     [:div#navigation-menu (use-style s-opened-navigation)
+      [:div (use-style s-nav-close)]
+      [:a (use-style s-nav-link {:href "#"
+                                 :data-testid "navigation__cards"
+                                 :on-click (fn [e]
+                                             (.preventDefault e)
+                                             (re-frame/dispatch [::events/set-search-tag nil])
+                                             (re-frame/dispatch [::events/set-search-archive false])
+                                             (re-frame/dispatch [::events/navigate :word-penne.pages.home/home]))})
+       [:span {:class "material-icons-outlined"} "style"]
+       [:span (use-style s-nav-link-text) (.gettext n "Cards")]]
+      (doall (map-indexed
+              (fn [index tag]
+                [:a (use-style s-nav-link {:href "#" :key index
+                                           :data-testid (str "navigation__tags-" index)
+                                           :on-click (fn [e]
+                                                       (.preventDefault e)
+                                                       (re-frame/dispatch [::events/set-search-tag tag]))})
+                 [:span {:class "material-icons-outlined"} "label"]
+                 [:span (use-style s-nav-link-text) tag]])
+              @(re-frame/subscribe [::subs/tags])))
+      [:a (use-style s-nav-link {:href (path-for routes :word-penne.pages.tags/index)})
+       [:span {:class "material-icons-outlined"} "edit"]
+       [:span (use-style s-nav-link-text) "Edit tags"]]
+      [:a (use-style s-nav-link {:href "#"
+                                 :data-testid "navigation__archive"
+                                 :on-click (fn [e]
+                                             (.preventDefault e)
+                                             (re-frame/dispatch [::events/set-search-archive true]))})
+       [:span {:class "material-icons-outlined"} "archive"]
+       [:span (use-style s-nav-link-text) "Archive"]]]]))
