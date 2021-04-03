272dea88fda3500514c213549994a03e44b3889f
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-04-03 21:14:15 +0900

    feat: seeds data

---
+++ src/main/tools/seeds.cljs
@@ -0,0 +1,34 @@
+(ns tools.seeds
+  (:require ["fs" :as fs]
+            ["firebase-admin" :as admin]))
+
+(def ^:private config
+  {:databaseURL "http://localhost:9100/firestore"
+   :user-uid "otJZ0cYxPtQlYnFYfJFSUHRlIJF4"})
+
+(defn- initialize-firebase []
+  (.initializeApp admin #js {:credential (-> admin .-credential .applicationDefault)
+                             :databaseURL (:databaseURL config)}))
+
+(defn- load-json-data []
+  (-> (.readFileSync fs "data/1_Basic300.json" "utf8")
+      (js/JSON.parse)
+      (js->clj)))
+
+(defn timestamp []
+  (.. admin -firestore -Timestamp now))
+
+(defn main [& cli-args]
+  (initialize-firebase)
+  (let [firestore (.firestore admin)]
+    (doseq [d (load-json-data)]
+      (-> firestore
+          (.collection (str "users/" (:user-uid config) "/cards"))
+          (.add #js {:front (d "name")
+                     :back (d "description")
+                     :archive false
+                     :lock false
+                     :tags #js []
+                     :createdAt (timestamp)
+                     :updatedAt (timestamp)})
+          (.then (fn [] nil))))))
