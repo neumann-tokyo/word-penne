5fda4cc587c3b67b91c3bb471a4d8d038c88d797
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-06-24 04:55:52 +0000

    feat: quit using focus-within

--- src/main/word_penne/components/avatar_menu.cljs
+++ src/main/word_penne/components/avatar_menu.cljs
@@ -1,6 +1,7 @@
 (ns word-penne.components.avatar-menu
   (:require [stylefy.core :as stylefy :refer [use-style]]
             [re-frame.core :as re-frame]
+            [reagent.core :as r]
             [word-penne.subs :as subs]
             [word-penne.events :as events]
             [word-penne.style.vars :refer [color z-indexs]]
@@ -14,8 +15,7 @@
    :background (:main-background color)
    :outline "none"
    :border "none"
-   :cursor "pointer"
-   ::stylefy/manual [[:&:focus-within [:#avatar-menu {:display "block"}]]]})
+   :cursor "pointer"})
 (def s-avatar
   {:vertical-align "middle"
    :width "2.5rem"
@@ -31,6 +31,7 @@
    :border (str "solid 1px " (:assort-border color))
    :border-radius "1rem"
    :color (:main-text color)
+   :text-align "center"
    :min-width "7rem"
    :box-shadow (str "0 2px 4px 0 " (:assort-border color))
    :overflow "hidden"})
@@ -41,20 +42,30 @@
    :padding ".5rem"
    ::stylefy/mode {:hover {:background (:assort-background color)}}})
 
+(def show-menu (r/atom false))
+
+(defn display-menu []
+  (if @show-menu
+    {:display "block"}
+    {:display "none"}))
+
 (defn AvatarMenu []
   (let [_ @(re-frame/subscribe [::subs/locale])
         {:keys [photo-url email]} @(re-frame/subscribe [::subs/current-user])]
     [:div (use-style s-menu)
-     [:button (use-style s-menu-button)
-      [:img (use-style s-avatar {:src photo-url :alt email})]
-      [:div#avatar-menu (use-style s-menu-content)
-       [:a (use-style s-menu-link) email]
-       [:a (use-style s-menu-link {:href "#"
-                                   :on-click (fn [e]
-                                               (.preventDefault e)
-                                               (re-frame/dispatch [::events/navigate :word-penne.pages.user/edit]))})
-        (tr "Settings")]
-       [:a (use-style s-menu-link {:href "#" :on-click (fn [e]
-                                                         (.preventDefault e)
-                                                         (re-frame/dispatch [::events/signout]))})
-        (tr "Logout")]]]]))
+     [:button (use-style s-menu-button {:on-click (fn [e]
+                                                    (.preventDefault e)
+                                                    (swap! show-menu not))})
+      [:img (use-style s-avatar {:src photo-url :alt email})]]
+     [:div (merge (use-style s-menu-content)
+                  {:style (display-menu)})
+      [:a (use-style s-menu-link) email]
+      [:a (use-style s-menu-link {:href "#"
+                                  :on-click (fn [e]
+                                              (.preventDefault e)
+                                              (re-frame/dispatch [::events/navigate :word-penne.pages.user/edit]))})
+       (tr "Settings")]
+      [:a (use-style s-menu-link {:href "#" :on-click (fn [e]
+                                                        (.preventDefault e)
+                                                        (re-frame/dispatch [::events/signout]))})
+       (tr "Logout")]]]))
