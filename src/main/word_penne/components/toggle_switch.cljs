7eb498f0a47a1d370f7c2b3876a03baefc5ca476
kbaba1001 <kbaba1001@users.noreply.github.com>
2021-06-26 20:52:06 +0000

    feat: add toggle_switch compornent

---
+++ src/main/word_penne/components/toggle_switch.cljs
@@ -0,0 +1,51 @@
+(ns word-penne.components.toggle-switch
+  (:require [stylefy.core :as stylefy :refer [use-style]]
+            [word-penne.style.vars :refer [color]]))
+
+(def s-switch
+  {:position "relative"
+   :display "inline-flex"
+   :align-items "center"
+   :height "34px"})
+(def s-checkbox
+  {:opacity 0
+   :width 0
+   :height 0
+   ::stylefy/manual [[:&:checked
+                      ["+ .slider" {:background-color (:accent-border color)}]
+                      ["+ .slider:before" {:transform "translateX(27px) rotate(-10deg)"
+                                           :background-color (:main-background color)}]]]})
+(def s-slider
+  {:position "absolute"
+   :cursor "pointer"
+   :display "inline-block"
+   :top 0
+   :left 0
+   :right 0
+   :bottom 0
+   :width "60px"
+   :background-color (:main-background color)
+   :transition ".4s"
+   :border-radius "15px 26px"
+   :border (str "solid 2px" (:accent-border color))
+   ::stylefy/mode {:before {:position "absolute"
+                            :content "''"
+                            :height "26px"
+                            :width "20px"
+                            :left "5px"
+                            :bottom "2px"
+                            :background-color (:accent-border color)
+                            :transition ".4s"
+                            :transform "rotate(-10deg)"
+                            :border-radius "15px 26px"}}})
+(def s-text
+  {:margin-left "66px"
+   :cursor "pointer"})
+
+(defn ToggleSwitch [attrs text]
+  [:label (use-style s-switch)
+   [:input
+    (use-style s-checkbox {:type "checkbox"
+                           :on-change (:on-change attrs)})]
+   [:span.slider (use-style s-slider)]
+   [:span (use-style s-text) text]])
--- src/main/word_penne/pages/home.cljs
+++ src/main/word_penne/pages/home.cljs
@@ -1,20 +1,30 @@
 (ns word-penne.pages.home
   (:require [re-frame.core :as re-frame]
+            [stylefy.core :as stylefy :refer [use-style]]
             [word-penne.views :as v]
             [word-penne.subs :as subs]
             [word-penne.i18n :refer [tr]]
             [word-penne.events :as events]
             [word-penne.components.button :refer [Button]]
             [word-penne.components.word-cards-wrap :refer [WordCardsWrap]]
-            [word-penne.components.confirmation-modal :refer [ConfirmationModal]]))
+            [word-penne.components.confirmation-modal :refer [ConfirmationModal]]
+            [word-penne.components.toggle-switch :refer [ToggleSwitch]]))
+
+(def s-container
+  {:display "flex"
+   :justify-content "space-between"
+   :align-items "center"
+   :margin-right ".5rem"})
 
 (defmethod v/view ::home [_]
   @(re-frame/subscribe [::subs/locale])
   [:div
-   [:div
+   [:div (use-style s-container)
     [Button {:kind "secondary" :on-click (fn [e]
                                            (.preventDefault e)
-                                           (re-frame/dispatch [::events/setup-quiz]))} (tr "Quiz")]]
+                                           (re-frame/dispatch [::events/setup-quiz]))} (tr "Quiz")]
+    [ToggleSwitch {:on-change (fn [e]
+                                (js/console.log "hihihi"))} (tr "Reverse")]]
    (when-let [tag @(re-frame/subscribe [::subs/search-tag])]
      [:p (str (tr "Tag: ") tag)])
    (when @(re-frame/subscribe [::subs/search-archive])
